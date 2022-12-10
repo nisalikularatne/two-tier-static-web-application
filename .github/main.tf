ffd37cd8864af193a636c62f86e66699eb2ebc88
nisali <nisalikularatne@gmail.com>
2022-12-10 00:34:21 -0500

    main.tf change

--- .github/main.tf
+++ .github/main.tf
@@ -1,18 +1,26 @@
 //Test if this code is failing
-resource "random_pet" "sg" {}
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-west-2"
 }
