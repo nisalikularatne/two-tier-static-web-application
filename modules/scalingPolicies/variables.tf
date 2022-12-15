412ccff1295cb09deaa03bc3b7e8141d45bfabb2
nisali <nisalikularatne@gmail.com>
2022-12-15 13:35:57 -0500

    Scaling policies added

---
+++ modules/scalingPolicies/variables.tf
@@ -0,0 +1,33 @@
+# Default tags
+variable "default_tags" {
+  default     = {}
+  type        = map(any)
+  description = "Default tags to be applied to all AWS resources"
+}
+# Variable to signal the current environment
+variable "env" {
+  default     = "dev"
+  type        = string
+  description = "Deployment Environment"
+}
+# Name prefix
+variable "prefix" {
+  type        = string
+  description = "Name prefix"
+}
+variable "scale_out_threshold" {
+  type        = number
+  description = "Scale out threshold"
+}
+variable "scale_in_threshold" {
+  type        = number
+  description = "Scale in threshold"
+}
+variable "scale_out_adjustment" {
+  type        = number
+  description = "Scale out adjustment"
+}
+variable "scale_in_adjustment" {
+  type        = number
+  description = "Scale in adjustment"
+}
