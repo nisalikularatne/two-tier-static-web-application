35515f3c72adcc902f8d0c6b753cab13cf6ffab4
EC2 Default User <ec2-user@ip-172-31-61-248.ec2.internal>
2022-12-18 17:41:07 +0000

    fix/modules environment issues

--- Project/prod/scalingPolicies/config.tf
+++ Project/prod/scalingPolicies/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "prod-acs730-finalproject-group15"       // Bucket where to SAVE Terraform State
+    bucket = "prod-acs730-finalproject-group15-ashish"       // Bucket where to SAVE Terraform State
     key    = "prod-scalingPolicies/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                              // Region where bucket is created
   }
