4eabf10ae9f3bb406434de99e6dd9f490fcedeea
Prajeshpuri <prajeshpuri@gmail.com>
2022-12-18 06:45:54 +0000

    PROD - Added TG and ASG

---
+++ Project/prod/autoScalingGroup/output.tf
@@ -0,0 +1,6 @@
+output "auto_scaling_group_id" {
+  value = module.autoScalingGroup-prod.auto_scaling_group_id
+}
+output "auto_scaling_group_name" {
+  value = module.autoScalingGroup-prod.auto_scaling_group_name
+}
