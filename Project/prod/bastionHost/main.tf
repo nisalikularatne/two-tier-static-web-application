f45fc4f234f36ea7cae6fdae0d98d4ad1a6b9e62
Ashish Fatnani <ashishfatnani3@gmail.com>
2022-12-21 03:18:26 -0500

    fix:image issue resolved (done)

--- Project/prod/bastionHost/main.tf
+++ Project/prod/bastionHost/main.tf
@@ -3,7 +3,7 @@
 module "bastion-prod" {
   source        = "../../../modules/bastionHost"
   prefix        = var.prefix
-  image         = var.image
+
   instance_type = var.instance_type
   linux_key_ec2 = var.linux_key_ec2
   bucket_name   = var.bucket_name
